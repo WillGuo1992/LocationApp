b5a30a0ed385501ec04ae7935dad7922fcee7eb7
will <285257188@qq.com>
2017-06-30 16:22:43 +0800

    log4j

--- test/WifiTest/TagLocTest.java
+++ test/WifiTest/TagLocTest.java
@@ -1,20 +1,11 @@
 package WifiTest;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-
 import org.apache.log4j.Logger;
 import org.junit.Test;
 
-import cn.buaa.edu.wifi.domain.MsgDevMac_V2;
-import cn.buaa.edu.wifi.service.impl.AMQReceiverG11;
-import cn.buaa.edu.wifi.service.impl.APTools;
-import cn.buaa.edu.wifi.service.impl.CSVReceiverG11;
 import cn.buaa.edu.wifi.service.impl.WifiIndoorTagLocMethod;
-
 public class TagLocTest {
+	
 	private static Logger logger = Logger.getLogger(WifiIndoorTagLocMethod.class);
 	
 	public static String Mac = "CC:3A:61:A9:09:53";
@@ -27,7 +18,6 @@ public class TagLocTest {
 	
 	@Test
 	public void get_csv() throws Exception{
-		
 		WifiIndoorTagLocMethod wifiIndoorTagLocMethod = new WifiIndoorTagLocMethod();
 		wifiIndoorTagLocMethod.getLocByTagMethodUsingFile(Mac);
 		
